4cae44839becc2b29c736f97b821d42d0c7c9406
Fraser Davidson <fraser@fdavidson.com>
2018-11-27 13:05:07 +0000

    Implement tests.

---
+++ test/Wipro.WebCrawler.App.Test/Helpers/UrlHelper/IsCrawlable.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Wipro.WebCrawler.App.Test.Helpers.UrlHelper
+{
+    [TestClass]
+    public class IsCrawlable
+    {
+        [TestMethod]
+        public void UrlHelper_IsCrawlable_ReturnsTrueForHttpUrl()
+        {
+            // Arrange.
+
+            var urlHelper = new App.Helpers.UrlHelper();
+
+            // Act.
+
+            var actual = urlHelper.IsCrawlable(new Uri("http://test.com"));
+
+            // Assert.
+
+            Assert.IsTrue(actual);
+        }
+
+        [TestMethod]
+        public void UrlHelper_IsCrawlable_ReturnsTrueForHttpsUrl()
+        {
+            // Arrange.
+
+            var urlHelper = new App.Helpers.UrlHelper();
+
+            // Act.
+
+            var actual = urlHelper.IsCrawlable(new Uri("https://test.com"));
+
+            // Assert.
+
+            Assert.IsTrue(actual);
+        }
+
+        [TestMethod]
+        public void UrlHelper_IsCrawlable_ReturnsFalseForNontHttpOrHttpsUrl()
+        {
+            // Arrange.
+
+            var urlHelper = new App.Helpers.UrlHelper();
+
+            // Act.
+
+            var actual = urlHelper.IsCrawlable(new Uri("file://test.com"));
+
+            // Assert.
+
+            Assert.IsFalse(actual);
+        }
+    }
+}
